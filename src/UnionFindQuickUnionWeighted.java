6f14cb7dc13d9ec9aa51a7d321630e8e3d612973
Szilveszter Szilagyi <sly99@gmx.com>
2017-10-21 16:09:41 +0100

    union find - quick union weighted

---
+++ src/UnionFindQuickUnionWeighted.java
@@ -0,0 +1,94 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+
+public class UnionFindQuickUnionWeighted {
+	public static final String INDENT = "               ";
+	public static final StringBuffer state2str(int[] state) {
+		StringBuffer str = new StringBuffer();
+		for (int i = 0; i < state.length; i++)
+			str.append(" " + state[i]);
+		return str;
+	}
+	private int[] id;
+	private int[] depth;
+
+	public UnionFindQuickUnionWeighted(int N) {
+		StdOut.println(INDENT + "initializing id, N operaton");
+		id = new int[N];
+		depth = new int[N];
+		for (int i = 0; i < N; i++)
+		{
+			id[i] = i;
+			depth[i] = 1;
+		}
+	}
+
+	public static void main(String[] args) {
+		int N = StdIn.readInt();
+		UnionFindQuickUnionWeighted uf = new UnionFindQuickUnionWeighted(N);
+		uf.printState();
+		while (!StdIn.isEmpty())
+		{
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+			StdOut.println(INDENT + "$ " + p + " " + q);
+			if (!uf.connected(p, q))
+			{
+				StdOut.println("union " + p + " and " + q);
+				uf.union(p, q);
+			}
+			else
+			{
+				StdOut.println(INDENT + p + " " + q + " are already connected");
+			}
+			uf.printState();
+		}
+	}
+
+	private void printState() {
+		StdOut.println(INDENT + "id[]   " + state2str(id));
+		StdOut.println(INDENT + "depth[]" + state2str(depth));
+	}
+
+	private int root(int i) {
+		StringBuffer log = new StringBuffer(INDENT + "root of " + i);
+		while (id[i] != i)
+		{
+			i = id[i];
+			log = log.append(" > " + i);
+		}
+		StdOut.println(log);
+		return i;
+	}
+
+	private void union(int p, int q) {
+		int rp = root(p);
+		int rq = root(q);
+		int dp = depth[rp];
+		int dq = depth[rq];
+		if (rp == rq) return;
+
+		// in this implementation depth only makes sense for top most nodes
+		// because keeping track of the real depth of all nodes is not necessary here
+		if (dp < dq)
+		{
+			StdOut.println(INDENT + "connect " + p + " to " + q + " by pointing " + rp +" to " + rq);
+			id[rp] = rq;
+			depth[rq] = Math.max(dq, dp + 1);
+		}
+		else
+		{
+			StdOut.println(INDENT + "connect " + q + " to " + p + " by pointing " + rq +" to " + rp);
+			id[rq] = rp;
+			depth[rp] = Math.max(dp, dq + 1);
+		}
+	}
+
+	private boolean connected(int p, int q) {
+		// lazy implementation, we could store or return the roots
+		// along with and the following union wouldn't need to lookup the roots again
+		return root(p) == root(q);
+	}
+
+}
